4668d613e5f826118f37acfcece4cf70498cfa28
Consolatrice-tech <65548460+Consolatrice-tech@users.noreply.github.com>
2020-10-25 06:51:05 +0200

    Created Blueprints and added the custom error pages

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,24 +1,33 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_mail import Mail 
+from flask_mail import Mail
+from flaskblog.config import Config
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '7524f665b44eeefccbe175a18cfe8938'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view='login'
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-app.config['Mail_SERVER'] = 'smtp.googlemail.com'
-app.config['Mail_PORT'] = 587
-app.config['Mail_USE_TLS'] = True
-app.config['Mail_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['Mail_PASSWORD'] = os.environ.get('EMAIL_PASS')
-mail = Mail(app)
+mail = Mail()
 
 
-from flaskblog import routes
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
